2cfed2f39ac4d0a2f02df464e3fb10496a97dd5d
Bria White <bria.awhite@gmail.com>
2020-08-06 03:33:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,5 @@ def prime?(number)
   return false if !number.integer?
   return false if number < 2
   return true if number == 2
-  (2..number-1).each {|num| return false if number % num ==0
+  (2..number-1).each {|num| return false if number % num ==0}
 end  
